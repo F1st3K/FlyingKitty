8a54d2ad536d57957d0bc17e49cf2d57e743ff9a
F1st3K <kostinnikita988@gmail.com>
2022-11-25 19:17:12 +0300

    create player and setposition player

--- FlyingKitty/FlyingKitty/MainWindow.xaml.cs
+++ FlyingKitty/FlyingKitty/MainWindow.xaml.cs
@@ -20,9 +20,23 @@ namespace FlyingKitty
     /// </summary>
     public partial class MainWindow : Window
     {
+        private Player _player;
+        private ObstacleControler _obsControler;
         public MainWindow()
         {
             InitializeComponent();
+            //load model player
+            Uri uriImage = new Uri("../../images/player.png", UriKind.Relative);
+            _player = new Player("player", 75, 75, new BitmapImage(uriImage));
+            MainCanvas.Children.Add(_player);
+            _player.SetPosition(75, 300);
+            //initialize obs controler and generate map
+            _obsControler = new ObstacleControler();
+
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
             
         }
     }
---
+++ FlyingKitty/FlyingKitty/Player.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace FlyingKitty
+{
+    internal class Player : Canvas
+    {
+        private Image _image;
+        private int posX, poxY;
+        public Player(string name, int width, int hight, ImageSource sourse)
+        {
+            Name = name;
+            Width = width;
+            Height = hight;
+            //load image
+            _image = new Image();
+            _image.Source = sourse;
+            _image.Width = Width;
+            _image.Height = Height;
+            Children.Add(_image);
+        }
+        public void SetPosition(int x, int y)
+        {
+            Canvas.SetLeft(this, x);
+            Canvas.SetTop(this, y);
+            posX = x;
+            poxY = y;
+        }
+    }
+}
