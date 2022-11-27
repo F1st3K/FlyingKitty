43889612e2daf21eb747e99b4bec215a696fe5a3
F1st3K <kostinnikita988@gmail.com>
2022-11-27 11:58:17 +0300

    refactoring (add class game && transfer game otiopn into it)

---
+++ FlyingKitty/FlyingKitty/Game.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Threading;
+
+namespace FlyingKitty
+{
+    internal class Game
+    {
+        public const double FPS = 120;
+        public const double G = 9.8;
+        public const double TICKRATE = 128;
+        static public int Tick { get; private set; } = 0;
+        
+        private bool IsGameOver = false;
+        private DispatcherTimer gameTimer = new DispatcherTimer();
+        private DispatcherTimer renderTimer = new DispatcherTimer();
+        private Player _player;
+        private ObstacleControler _obsControler;
+        public Game(Player player)
+        {
+            _player = player;
+        }
+        public void Start()
+        {
+            //create game ticrate timer   
+            gameTimer.Interval = TimeSpan.FromSeconds(1 / TICKRATE);
+            gameTimer.Tick += new EventHandler(Update);
+            gameTimer.Start();
+            //create render FPS timer
+            renderTimer.Interval = TimeSpan.FromSeconds(1 / FPS);
+            renderTimer.Tick += new EventHandler(Render);
+            renderTimer.Start();
+        }
+        private void Update(object sender, EventArgs e)
+        {
+            _player.Update();
+            Tick++;
+        }
+        private void Render(object sender, EventArgs e)
+        {
+            _player.RenderPosition();
+        }
+        private void End()
+        {
+
+        }
+    }
+}
--- FlyingKitty/FlyingKitty/MainWindow.xaml.cs
+++ FlyingKitty/FlyingKitty/MainWindow.xaml.cs
@@ -23,74 +23,37 @@ namespace FlyingKitty
     /// </summary>
     public partial class MainWindow : Window
     {
-        private Player _player;
+        private Player player;
         private ObstacleControler _obsControler;
-
-        private bool IsGameOver = false;
-        private DispatcherTimer gameTimer = new DispatcherTimer();
-        static public double tickRate = 128;
-        private int tick = 0;
-        private int KeyTime = 0;
-        private DispatcherTimer renderTimer = new DispatcherTimer();
-        static private double FPS = 120;
-
-        static public double g = 9.8;
-
+        private Game game;
 
         public MainWindow()
         {
             InitializeComponent();
             //load model player
             Uri uriImage = new Uri("../../images/player.png", UriKind.Relative);
-            _player = new Player(90*g/tickRate, 360, 75, 75, new BitmapImage(uriImage));
-            MainCanvas.Children.Add(_player);
-            _player.SetPosition(75, 400);
-            _player.RenderPosition();
+            player = new Player(90*Game.G/Game.TICKRATE, 360, 75, 75, new BitmapImage(uriImage));
+            MainCanvas.Children.Add(player);
+            player.SetPosition(75, 400);
+            player.RenderPosition();
             //initialize obs controler and generate map
             _obsControler = new ObstacleControler();
-            Start();
-        }
-        private void Start()
-        {
-            //create game ticrate timer   
-            gameTimer.Interval = TimeSpan.FromSeconds(1/tickRate);
-            gameTimer.Tick += new EventHandler(Update);
-            gameTimer.Start();
-            //create render FPS timer
-            renderTimer.Interval = TimeSpan.FromSeconds(1 / FPS);
-            renderTimer.Tick += new EventHandler(Render);
-            renderTimer.Start();
-        }
-
-        private void Render(object sender, EventArgs e)
-        {
-            _player.RenderPosition();
-        }
-
-        private void Update(object sender, EventArgs e)
-        {
-            _player.Update();
-            tick++;
-        }
-
-        private void End()
-        {
-
+            //start game
+            game = new Game(player);
+            game.Start();
         }
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
             
         }
-
-
-        private void KeyDownEvent(object sender, KeyEventArgs e)
+        private new void KeyDownEvent(object sender, KeyEventArgs e)
         {
             if (e.Key == Key.Enter ||
                 e.Key == Key.Space ||
                 e.Key == Key.Down)
             {
-                if (_player.DirectionY == -1)
-                    _player.IsPushDown = true;
+                if (player.DirectionY == -1)
+                    player.IsPushDown = true;
             }
         }
     }
