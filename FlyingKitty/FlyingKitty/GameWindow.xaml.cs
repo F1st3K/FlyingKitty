1d438af399b4508f5967aaf07f635d6713657510
F1st3K <kostinnikita988@gmail.com>
2022-11-30 18:48:49 +0300

    removal of functionality in GameWindow

---
+++ FlyingKitty/FlyingKitty/GameWindow.xaml.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace FlyingKitty
+{
+    /// <summary>
+    /// Логика взаимодействия для GameWindow.xaml
+    /// </summary>
+    public partial class GameWindow : Window
+    {
+        private Game game;
+
+        public GameWindow()
+        {
+            InitializeComponent();
+            game = new Game();
+        }
+
+        private void LoadLevel()
+        {
+            //load map settings
+            ObstacleControler.GameSpeed = 500 / Game.TICKRATE;
+            ObstacleControler.WidthObjects = 80;
+            ObstacleControler.GapObjects = 10;
+            ObstacleControler.HeigthWin = 200;
+            ObstacleControler.ApetureWin = 50;
+            game.LoadMap("_______12_34_56_78_98_7_6_5_4_3_2_1___");
+            CreateModel();
+            LoadModel();
+        }
+        private void LoadModel()
+        {
+            //add obstacle on MainCanvas
+            for (int i = 0; i < ObstacleControler.Ground.Length; i++)
+                MainCanvas.Children.Add(ObstacleControler.Ground[i]);
+            MainCanvas.Children.Add(ObstacleControler.Sky);
+            MainCanvas.Children.Add(ObstacleControler.Finish);
+            for (int i = 0; i < ObstacleControler.Map.Length; i++)
+                MainCanvas.Children.Add(ObstacleControler.Map[i]);
+            MainCanvas.Children.Add(game._player);
+        }
+        private void CreateModel()
+        {
+            //Create obstacle
+            ObstacleControler.SetGround((int)Width, 50, "../../media/graund.jpg");
+            ObstacleControler.SetSky((int)Width, 10, "../../media/sky.jpg");
+            ObstacleControler.SetFinish(350, (int)Height, "../../media/finish.jpg");
+            ObstacleControler.SetMap(75, (int)Height, "../../media/obstacle.png");
+            //load model player
+            game.CreatePlayer(70, 200, 50, 105, 50, 44, "../../media/kitty.png", "../../media/boll.png");
+        }
+        private void RestartGame()
+        {
+            MainCanvas.Children.Clear();
+            game.Stop();
+            game = new Game();
+            LoadLevel();
+            game.LoadSound();
+            game.Start();
+        }
+        private new void KeyDownEvent(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Space ||
+                e.Key == Key.Down)
+                game.PressDown();
+            if (e.Key == Key.Enter)
+                RestartGame();
+        }
+    }
+}
+
--- FlyingKitty/FlyingKitty/MainWindow.xaml.cs
+++ FlyingKitty/FlyingKitty/MainWindow.xaml.cs
@@ -13,58 +13,13 @@ namespace FlyingKitty
         public MainWindow()
         {
             InitializeComponent();
-            game = new Game();
         }
 
-        private void LoadLevel()
+        private void start_Click(object sender, RoutedEventArgs e)
         {
-            //load map settings
-            ObstacleControler.GameSpeed = 500 / Game.TICKRATE;
-            ObstacleControler.WidthObjects = 80;
-            ObstacleControler.GapObjects = 10;
-            ObstacleControler.HeigthWin = 200;
-            ObstacleControler.ApetureWin = 50;
-            game.LoadMap("_______12_34_56_78_98_7_6_5_4_3_2_1___");
-            CreateModel();
-            LoadModel();
-        }
-        private void LoadModel()
-        {
-            //add obstacle on MainCanvas
-            for (int i = 0; i < ObstacleControler.Ground.Length; i++)
-                MainCanvas.Children.Add(ObstacleControler.Ground[i]);
-            MainCanvas.Children.Add(ObstacleControler.Sky);
-            MainCanvas.Children.Add(ObstacleControler.Finish);
-            for (int i = 0; i < ObstacleControler.Map.Length; i++)
-                MainCanvas.Children.Add(ObstacleControler.Map[i]);
-            MainCanvas.Children.Add(game._player);
-        }
-        private void CreateModel()
-        {
-            //Create obstacle
-            ObstacleControler.SetGround((int)Width, 50, "../../media/graund.jpg");
-            ObstacleControler.SetSky((int)Width, 10, "../../media/sky.jpg");
-            ObstacleControler.SetFinish(350, (int)Height, "../../media/finish.jpg");
-            ObstacleControler.SetMap(75, (int)Height, "../../media/obstacle.png");
-            //load model player
-            game.CreatePlayer(70, 200, 50, 105, 50, 44, "../../media/kitty.png", "../../media/boll.png");
-        }
-        private void RestartGame()
-        {
-            MainCanvas.Children.Clear();
-            game.Stop();
-            game = new Game();
-            LoadLevel();
-            game.LoadSound();
-            game.Start();
-        }
-        private new void KeyDownEvent(object sender, KeyEventArgs e)
-        {
-            if (e.Key == Key.Space ||
-                e.Key == Key.Down)
-                game.PressDown();
-            if (e.Key == Key.Enter)
-                RestartGame();
+            this.Hide();
+            GameWindow gameWindow = new GameWindow();
+            gameWindow.Show();
         }
     }
 }
